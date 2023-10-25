4951a3e73ebbaf9cd782f7f9acf625484b45cde7
Darren Fife <fifeda@mail.gvsu.edu>
2023-10-25 16:38:48 -0400

    Implemented checking valid yt urls

--- pytube_code.py
+++ pytube_code.py
@@ -1,9 +1,31 @@
 import pytube as pyt
-from pytube import extract
+from pytube import YouTube, Playlist, Channel, extract
+from pytube.exceptions import VideoUnavailable
+from pytube.exceptions import RegexMatchError
 import os
 
 
-class Video(pyt.YouTube):
+class InvalidURLException(Exception):
+    """Raised when URL is not valid"""
+    pass
+
+
+class InvalidVideoException(InvalidURLException):
+    """Raised when URL is not a valid Video"""
+    pass
+
+
+class InvalidPlaylistException(InvalidURLException):
+    """Raised when URL is not a valid Playlist"""
+    pass
+
+
+class InvalidChannelException(InvalidURLException):
+    """Raised when URL is not a valid Channel"""
+    pass
+
+
+class Video(YouTube):
     """A video uploaded to YouTube.
 
     Keyword arguments:
@@ -11,11 +33,21 @@ class Video(pyt.YouTube):
     # URL Parameter constructor
     def __init__(self, url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"):
         """Construct a Video object using the video uploaded to the given YouTube link."""
-        super().__init__(url)
-        self.channel_name = pyt.Channel(self.channel_url).channel_name
+        try:
+            extract.video_id(url)
+        except RegexMatchError as e:
+            raise InvalidVideoException from e
+        else:
+            try:
+                super().__init__(url)
+            except VideoUnavailable:
+                print(f'Video {url} is unavailable, skipping.')
+                raise VideoUnavailable
+            else:
+                print(f'Creating video object: {url}')
+                self.channel_name = pyt.Channel(self.channel_url).channel_name
 
-
-    # Downloads Set Video to Project Folder
+    """Downloads Set Video to Project Folder"""
     def download_video(self, max_res=720):
         """Download a video from a YouTube link.
 
@@ -54,3 +86,65 @@ class Video(pyt.YouTube):
         print("Video downloaded: " + path + self.title + " with ID: " + self.video_id)
 
     pass
+
+
+class YDPlaylist(Playlist):
+    def __init__(self, url):
+        if "list=" not in url:
+            raise InvalidPlaylistException
+        else:
+            super().__init__(url)
+
+            self.yd_list = []
+
+            for url in super().video_urls:
+                self.yd_list.append(Video(url))
+
+    def download_playlist(self, max_res=720):
+        for video in self.yd_list:
+            video.download_video(max_res)
+
+
+class YDChannel(Channel):
+    def __init__(self, url):
+        try:
+            extract.channel_name(url)
+        except RegexMatchError as e:
+            raise InvalidChannelException from e
+        else:
+            super().__init__(url)
+
+            self.yd_list = []
+
+            for url in super().video_urls:
+                self.yd_list.append(Video(url))
+
+    def download_channel(self, max_res=720):
+        for video in self.yd_list:
+            video.download_video(max_res)
+
+
+def download_link(url):
+    """Download a YouTube link by turning it into the right type of object"""
+    try:
+        c = YDChannel(url)
+    except InvalidChannelException:
+        print("Invalid Channel URL")
+        try:
+            p = YDPlaylist(url)
+        except InvalidPlaylistException:
+            print("Invalid Playlist URL")
+            try:
+                v = Video(url)
+            except InvalidVideoException:
+                print("Invalid Video URL")
+            else:
+                print("Valid Video " + url)
+                v.download_video()
+        else:
+            print("Valid Playlist " + url)
+            p.download_playlist()
+    else:
+        print("Valid Channel " + url)
+        c.download_channel()
+
