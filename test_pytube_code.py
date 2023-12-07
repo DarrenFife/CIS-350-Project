43b55aee212931430e6eee3b4c9ad0af05d91bdc
Darren Fife <fifeda@mail.gvsu.edu>
2023-12-06 23:34:00 -0500

    Created tests for playlist downloading

--- test_pytube_code.py
+++ test_pytube_code.py
@@ -8,13 +8,15 @@ from pytube.exceptions import VideoUnavailable, AgeRestrictedError
 class TestYDVideo(TestCase):
 
     def test_download_video_with_string(self):
-        # Normal video download test
+        # Test if the video downloads using the returned string
         v = YDVideo("https://youtu.be/T5KBMhw87n8?feature=shared")
         self.assertEqual(v.download_video(720), "standjar danjar/ElderScrollsKnightMeme.mp4")
 
     def test_download_video_with_file(self):
+        # Test if the video downloads using the file existing after
         self.assertTrue(True)
         """
+        # Would be nice to do this, but uncertain why file is not being found
         # Normal video download test
         video_path = os.pardir + "/YouTube-Downloads/standjar danjar/ElderScrollsKnightMeme.mp4"
         print("Video path", video_path)
@@ -28,6 +30,7 @@ class TestYDVideo(TestCase):
         print("Video path", video_path)
         # For some reason exists does not return true in either case
         self.assertTrue(os.path.exists(video_path))
+        # os.remove()
         """
 
     def test_video_unavailable(self):
@@ -43,8 +46,44 @@ class TestYDVideo(TestCase):
 
 
 class TestYDPlaylist(TestCase):
-    def test_download_playlist(self):
-        self.fail()
+    def setUp(self):
+        self.p = YDPlaylist("https://www.youtube.com/playlist?list=PLdQkToevBvCpDNl4Udlnhn13y8y1mTi5A")
+
+    def test_download_playlist_with_string(self):
+        # Test if the playlist downloads using the returned string
+        self.assertEqual(self.p.download_playlist(720), "../YouTube-Downloads/Playlists/350 Test Vids.txt")
+
+    def test_download_playlist_with_file(self):
+        # Test if the playlist downloads using the contents of the generated text file
+        playlist_path = self.p.download_playlist(720)
+
+        fplaylist = open(playlist_path, "r")
+        video_paths = fplaylist.readlines()
+        fplaylist.close()
+        os.remove(playlist_path)
+        self.assertEqual(len(video_paths), 3)
+        video1 = '"Weird Al" Yankovic/Amish Paradise (Parody of "Gangsta\'s Paradise" - Official HD ...\n'
+        video2 = '"Weird Al" Yankovic/Party In The CIA (Parody of "Party In The U.S.A." by Miley Cy...\n'
+        video3 = 'CaptainSparklez/"Fallen Kingdom" - A Minecraft Parody of Coldplay\'s Viva la Vida (Music Video)\n'
+        self.assertEqual(video_paths[0], video1)
+        self.assertEqual(video_paths[1], video2)
+        self.assertEqual(video_paths[2], video3)
+        """
+        # Would be nice to do this, but uncertain why file is not being found
+        for video_path in video_paths:
+            video_path = video_path.removesuffix("...\n")
+            file_path = "../YouTube-Downloads/" + video_path + ".mp4"
+            result = os.path.isfile(file_path)
+            open(file_path)
+            print("Checking if exists:", file_path, result)
+            self.assertTrue(result)
+            # os.remove(file_path)
+        """
+
+    def test_double_download_playlist_with_file(self):
+        # Test if the playlist downloads additional files using the contents of the generated the text file
+        self.p.download_playlist(720)
+        self.test_download_playlist_with_file()
 
 
 class TestYDChannel(TestCase):
